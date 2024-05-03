46fb58d79f2be32d373ca82c7820a0877cf8cad8
Lilyana Ivanova <lennibossta@gmail.com>
2024-05-03 13:34:07 +0300

    Update 02. Football Results.py

--- 02. Football Results.py
+++ 02. Football Results.py
@@ -1,17 +1,47 @@
-result_first_match = input().split(":")
-result_second_match = input().split(":")
-result_third_match = input().split(":")
+result_first_match = input()
+result_second_match = input()
+result_third_match = input()
+# initialize counter for won, lost and drawn
+won = 0
+lost = 0
+drawn = 0
 
-if result_first_match > 3:
-    print("Team won 1 games")
+# check for the first match
+if int(result_first_match[0]) > int(result_first_match[2]):
+    won += 1
 
-elif result_first_match < 0:
-    print("Team lost 1 games")
+elif int(result_first_match[0]) < int(result_first_match[2]):
+    lost += 1
 
-elif result_third_match == 0:
-    print("Drawn games: 1")
+else:
+    drawn += 1
 
+# check for the second match
+if int(result_second_match[0]) > int(result_second_match[2]):
+    won += 1
 
+elif int(result_second_match[0]) < int(result_second_match[2]):
+    lost += 1
+
+else:
+    drawn += 1
+
+# check for the third match
+if int(result_third_match[0]) > int(result_third_match[2]):
+    won += 1
+
+elif int(result_third_match[0]) < int(result_third_match[2]):
+    lost += 1
+
+else:
+    drawn += 1
+# result
+print(f"Team won {won} games.")
+print(f"Team lost {lost} games.")
+print(f"Drawn games: {drawn}")
+# •	"Team won {брой спечелени мачове} games."
+# •	"Team lost {брой загубени мачове} games."
+# •	" Drawn games: {брой равни мачове}"
 
 # Team won 1 games.
 # Team lost 1 games.
